d10207479801b33170a92af34fb480925c4675e1
Jean-Lionel <nijeanlionel@gmail.com>
2022-11-11 17:22:48 +0200

    difine role of users

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +27,15 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        //        
+        Gate::define('is-admin', function (User $user) {
+            return $user->isAdmin();
+        });
+        Gate::define('is-client', function (User $user) {
+            return $user->isClient();
+        }); 
+        Gate::define('is-agent', function (User $user) {
+            return $user->isAgent();
+        });
     }
 }
