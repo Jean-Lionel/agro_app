e15087a900623f5d695b3a494ffd63c406b4633d
Jean-Lionel <nijeanlionel@gmail.com>
2022-11-11 15:45:06 +0200

    add agent create

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,4 +2,41 @@
 
 @section('content')
 
+<div>
+	
+	<!-- Add Modal -->
+	<div class="" id="Add_Specialities_details" aria-hidden="true" role="dialog">
+		<div class="modal-dialog modal-dialog-centered" role="document" >
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title">Ajouter un Agent</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<form>
+						<div class="row form-row">
+							<div class="col-12 col-sm-6">
+								<div class="form-group">
+									<label>Specialities</label>
+									<input type="text" class="form-control">
+								</div>
+							</div>
+							<div class="col-12 col-sm-6">
+								<div class="form-group">
+									<label>Image</label>
+									<input type="file"  class="form-control">
+								</div>
+							</div>
+
+						</div>
+						<button type="submit" class="btn btn-primary btn-block">Save Changes</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
 @stop
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,56 +2,65 @@
 
 @section('content')
 
-<div>
-	
-	<div class="card">
-		<div class="card-body">
+<div class="content container-fluid">
 
-			<div>
-				<a href="{{route('users.create')}}">Ajouter un Agent</a>
+	<!-- Page Header -->
+	<div class="page-header">
+		<div class="row">
+			<div class="col-sm-7 col-auto">
+				<h3 class="page-title">Specialities</h3>
+				<ul class="breadcrumb">
+					<li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+					<li class="breadcrumb-item active">Specialities</li>
+				</ul>
 			</div>
-			<div class="table-responsive">
-				<div id="DataTables_Table_0_wrapper" class="dataTables_wrapper dt-bootstrap4 no-footer"><div class="row"><div class="col-sm-12 col-md-6"><div class="dataTables_length" id="DataTables_Table_0_length"><label>Show <select name="DataTables_Table_0_length" aria-controls="DataTables_Table_0" class="custom-select custom-select-sm form-control form-control-sm"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select> entries</label></div></div><div class="col-sm-12 col-md-6"></div></div><div class="row"><div class="col-sm-12">
-
-					<table class="datatable table table-hover table-center mb-0 dataTable no-footer" id="DataTables_Table_0" role="grid" aria-describedby="DataTables_Table_0_info">
-					<thead>
-						<tr role="row"><th class="sorting_asc" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Doctor Name: activate to sort column descending" style="width: 261.688px;">Doctor Name</th><th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Speciality: activate to sort column ascending" style="width: 115.75px;">Speciality</th><th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Member Since: activate to sort column ascending" style="width: 157.203px;">Member Since</th><th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Earned: activate to sort column ascending" style="width: 85.4062px;">Earned</th><th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Account Status: activate to sort column ascending" style="width: 168.953px;">Account Status</th></tr>
-					</thead>
-
-					<tbody>
-						
-					@foreach ($users as $element)
-						{{-- expr --}}
-						<tr role="row" class="odd">
-							<td class="sorting_1">
-								<h2 class="table-avatar">
-									<a href="profile.html" class="avatar avatar-sm mr-2"><img class="avatar-img rounded-circle" src="assets/img/doctors/doctor-thumb-02.jpg" alt="User Image"></a>
-									<a href="profile.html">{{$element->prenom   }} {{ $element->nom }}</a>
-								</h2>
-							</td>
-							<td>{{$element->id}}</td>
-							
-							<td>{{$element->nom}}</td>
-							<td>{{$element->prenom}}</td>
-							<td>{{$element->name}}</td>
-							<td>{{$element->email}}</td>
-							
-							
-							<td>
-								<div class="status-toggle">
-									<input type="checkbox" id="status_1" class="check" checked="">
-									<label for="status_1" class="checktoggle">checkbox</label>
-								</div>
-							</td>
-						</tr>
-					@endforeach
-
-						
-					</tbody>
-
-					</table></div></div><div class="row"><div class="col-sm-12 col-md-5"><div class="dataTables_info" id="DataTables_Table_0_info" role="status" aria-live="polite">Showing 1 to 10 of 10 entries</div></div><div class="col-sm-12 col-md-7"><div class="dataTables_paginate paging_simple_numbers" id="DataTables_Table_0_paginate"><ul class="pagination"><li class="paginate_button page-item previous disabled" id="DataTables_Table_0_previous"><a href="#" aria-controls="DataTables_Table_0" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li><li class="paginate_button page-item active"><a href="#" aria-controls="DataTables_Table_0" data-dt-idx="1" tabindex="0" class="page-link">1</a></li><li class="paginate_button page-item next disabled" id="DataTables_Table_0_next"><a href="#" aria-controls="DataTables_Table_0" data-dt-idx="2" tabindex="0" class="page-link">Next</a></li></ul></div></div></div></div>
-				</div>
+			<div class="col-sm-5 col">
+				<a href="{{ route('users.create') }} " class="btn btn-primary float-right mt-2">Add</a>
 			</div>
 		</div>
 	</div>
+	<!-- /Page Header -->
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="card">
+				<div class="card-body">
+					<div class="table-responsive">
+						<div id="DataTables_Table_0_wrapper" class="dataTables_wrapper dt-bootstrap4 no-footer"><div class="row"><div class="col-sm-12 col-md-6"><div class="dataTables_length" id="DataTables_Table_0_length"><label>Show <select name="DataTables_Table_0_length" aria-controls="DataTables_Table_0" class="custom-select custom-select-sm form-control form-control-sm"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select> entries</label></div></div><div class="col-sm-12 col-md-6"></div></div><div class="row"><div class="col-sm-12"><table class="datatable table table-hover table-center mb-0 dataTable no-footer" id="DataTables_Table_0" role="grid" aria-describedby="DataTables_Table_0_info">
+							<thead>
+								<tr role="row"><th class="sorting_asc" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-sort="ascending" aria-label="#: activate to sort column descending" style="width: 162.156px;">#</th><th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Specialities: activate to sort column ascending" style="width: 352.734px;">Specialities</th><th class="text-right sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Actions: activate to sort column ascending" style="width: 358.109px;">Actions</th></tr>
+							</thead>
+							<tbody>
+								
+								@foreach ($users as $element)
+									{{-- expr --}}
+
+								<tr role="row" class="odd">
+									<td class="sorting_1">{{ $element->id }}</td>
+									<td class="sorting_1">{{ $element->name }}</td>
+									<td class="sorting_1">{{ $element->nom }}</td>
+									<td class="sorting_1">{{ $element->prenom }}</td>
+
+									
+									<td class="text-right">
+										<div class="actions">
+											<a class="btn btn-sm bg-success-light" data-toggle="modal" href="#edit_specialities_details">
+												<i class="fe fe-pencil"></i> Edit
+											</a>
+											<a data-toggle="modal" href="#delete_modal" class="btn btn-sm bg-danger-light">
+												<i class="fe fe-trash"></i> Delete
+											</a>
+										</div>
+									</td>
+								</tr>
+								@endforeach
+							</tbody>
+						</div></div><div class="row"><div class="col-sm-12 col-md-5"><div class="dataTables_info" id="DataTables_Table_0_info" role="status" aria-live="polite">Showing 1 to 5 of 5 entries</div></div><div class="col-sm-12 col-md-7"><div class="dataTables_paginate paging_simple_numbers" id="DataTables_Table_0_paginate"><ul class="pagination"><li class="paginate_button page-item previous disabled" id="DataTables_Table_0_previous"><a href="#" aria-controls="DataTables_Table_0" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li><li class="paginate_button page-item active"><a href="#" aria-controls="DataTables_Table_0" data-dt-idx="1" tabindex="0" class="page-link">1</a></li><li class="paginate_button page-item next disabled" id="DataTables_Table_0_next"><a href="#" aria-controls="DataTables_Table_0" data-dt-idx="2" tabindex="0" class="page-link">Next</a></li></ul></div></div></div></div>
+						</div>
+					</div>
+				</div>
+			</div>			
+		</div>
+	</div>
+
+
 	@stop
